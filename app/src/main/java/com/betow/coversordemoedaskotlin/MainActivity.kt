f4d892f626c3f043986d185df8c06440acbff901
Beto Drumond <humbertowdrumond@gmail.com>
2021-07-04 17:13:33 -0300

    New commit

--- app/src/main/java/com/betow/coversordemoedaskotlin/MainActivity.kt
+++ app/src/main/java/com/betow/coversordemoedaskotlin/MainActivity.kt
@@ -5,22 +5,30 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var campo1: EditText
-    lateinit var botao1: Button
+    lateinit var etValorDolar: EditText
+    lateinit var etQtdDolar: EditText
+    lateinit var tvValorConvertido: TextView
+    lateinit var btConverter: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        campo1 = findViewById(R.id.nome)
-        botao1 = findViewById(R.id.botao)
-        botao1.setOnClickListener(object : View.OnClickListener{
+
+        etValorDolar = findViewById(R.id.edtxValorDolar)
+        etQtdDolar = findViewById(R.id.edtxQtdadeDolar)
+        tvValorConvertido = findViewById(R.id.txtResultadoConvertido)
+        btConverter = findViewById(R.id.botao)
+
+
+        btConverter.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                var textoDigitado = campo1.text.toString()
-                Toast.makeText(applicationContext, textoDigitado, Toast.LENGTH_LONG).show()
+                var digitado = etValorDolar.text.toString()
+                Toast.makeText(applicationContext, digitado, Toast.LENGTH_LONG).show()
             }
 
         })
